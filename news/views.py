8b61d9a5dea72ae263f1aa49acb3fff821f2977e
manal lahyani <manalahyani@gmail.com>
2020-06-12 17:55:13 +0000

    fst repo

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News
 from main.models import Main
+import datetime
 
 
 
@@ -19,6 +20,17 @@ def news_list(request):
     return render(request,'back/news_list.html',{'news':news})
 
 def add_news(request):
-    print('okokokokokokoko')
+    if request.method=='POST':
+        newstitle=request.POST.get('newstitle')
+        newscat=request.POST.get('newscat')
+        newsdesc=request.POST.get('newsdesc')
+        newstxt=request.POST.get('newstxt')
+
+        if newstitle=='' or newscat=='' or newsdesc=='' or newstxt=='':
+            error='ALL FIELD REQUIED'
+            return render(request,'back/error.html',{'error':error})
+        b=News(name=newstitle, catname=newscat, desc=newsdesc, txt_body=newstxt, date=datetime.date, pic="-", writer='-', views=0, catid=0)
+        b.save()
+        return redirect('news_list')
     return render(request,'back/add_news.html')
 
